e9d061f373e3ff78da00eb149d7ee91aa31ef28e
James Rhodes <jrhodes@redpointsoftware.com.au>
2012-05-01 20:06:53 +1000

    Added empty library for symbol table format.

---
+++ libdcpu-ci-dbgfmt/ddata.c
@@ -0,0 +1,14 @@
+/**
+
+	File:			ddata.c
+
+	Project:		DCPU-16 Tools
+	Component:		LibDCPU-CI-DbgFmt
+
+	Authors:		James Rhodes
+
+	Description:	
+
+**/
+
+#include "ddata.h"
---
+++ libdcpu-ci-dbgfmt/ddata.h
@@ -0,0 +1,17 @@
+/**
+
+	File:			ddata.h
+
+	Project:		DCPU-16 Tools
+	Component:		LibDCPU-CI-DbgFmt
+
+	Authors:		James Rhodes
+
+	Description:	
+
+**/
+
+#ifndef __DCPU_DBG_DDATA_H
+#define __DCPU_DBG_DDATA_H
+
+#endif
