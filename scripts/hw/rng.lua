ab29c1de1c25c59a1a93eee8fae433ddef93973f
James Rhodes <jrhodes@redpointsoftware.com.au>
2012-05-18 19:54:34 +1000

    Updated random number generator hardware.

--- scripts/hw/rng.lua
+++ scripts/hw/rng.lua
@@ -1,26 +1,36 @@
+-- if the emulator is at a path like: path/to/dtemu.exe
+-- then put this in a path like:      path/to/hw/rng.lua
+
 -- interrupt values
 local INT_GENERATE = 0;
 local INT_SEED = 1;
 
 function interrupt(cpu)
   -- cpu is a table that lets you do things to the CPU.
-  --if (cpu.registers.A == INT_GENERATE) then
-  --  cpu.registers.B = math.random(0x0, 0xFFFF);
-  --elseif (cpu.registers.A == INT_SEED) then
-  --  math.randomseed(cpu.registers.B);
-  --end
+  if (cpu.registers.A == INT_GENERATE) then
+    cpu.registers.B = math.random(0x0, 0xFFFF);
+  elseif (cpu.registers.A == INT_SEED) then
+    math.randomseed(cpu.registers.B);
+  end
 end
 
 function cycle(cpu)
   -- cpu is a table that lets you do things to the CPU.
-  -- in accordance with hardware standards, this is not called
-  -- until the first interrupt has been received and handled.
-  print(string.format("0x%X", cpu.ram[0x0]))
+  -- according to hardware standards, this is not called
+  -- until the first interrupt has been received and handled,
+  -- however at the moment the toolchain ignores this
+  -- requirement and it's called anyway :P
+  
+  --[[print(string.format("0x%X", cpu.ram[0x0]))
   print(cpu.registers.Z)
   print(cpu.registers.PC)
   cpu.registers.Z = math.random(0x0, 0xFFFF);
-  cpu.ram[0x0] = 0x0;
-  --print("cycling on random number generator")
+  cpu.ram[0x0] = 0x0;]]--
+end
+
+function write(cpu, pos)
+  -- cpu is a table that lets you do things to the CPU.
+  -- pos is the memory address that was written to.
 end
 
 MODULE = {
