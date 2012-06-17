6aed07ce13b90451de431f8f26d0f803749f862d
James Rhodes <jrhodes@redpointsoftware.com.au>
2012-06-17 22:20:35 +1000

    Improved and added standalone Lua interpreter.
    Added binary and octal number support to Lua interpreter (via power patch).
    Added "continue" support to Lua interpreter (via power patch).
    Added left shift (<<), right shift (>>), binary or (|), binary and (&), binary xor (^|), binary not (~) and integer division (//) operators to Lua.

--- third-party/lua/ltm.c
+++ third-party/lua/ltm.c
@@ -30,13 +30,16 @@ const char* const luaT_typenames[] =
 
 void luaT_init(lua_State* L)
 {
-	static const char* const luaT_eventname[] =  	 /* ORDER TM */
+	static const char* const luaT_eventname[] =	 /* ORDER TM */
 	{
 		"__index", "__newindex",
 		"__gc", "__mode", "__eq",
 		"__add", "__sub", "__mul", "__div", "__mod",
 		"__pow", "__unm", "__len", "__lt", "__le",
 		"__concat", "__call"
+#ifdef LUA_BITWISE_OPERATORS
+		, "__or", "__and", "__xor", "__shl", "__shr", "__not", "__intdiv"
+#endif
 	};
 	int i;
 	for (i = 0; i < TM_N; i++)
