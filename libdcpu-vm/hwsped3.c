ef541bf23f5c921c994a41b41f6fea13eaa33bcd
José Manuel Díez <jose.manuel.diez@gmail.com>
2012-10-11 22:25:49 +0200

    Retabbed hwsped3.c

--- libdcpu-vm/hwsped3.c
+++ libdcpu-vm/hwsped3.c
@@ -1,12 +1,12 @@
 /**
 
-    File:	hwsped3.c
+    File:   hwsped3.c
 
-    Project:	DCPU-16 Tools
-    Component:	LibDCPU-vm
+    Project:    DCPU-16 Tools
+    Component:  LibDCPU-vm
 
-    Authors:	Jose Manuel Diez
-		David Herberth
+    Authors:    Jose Manuel Diez
+        David Herberth
 
     Description: Implements the SPED-3 specification.
 **/
@@ -45,8 +45,8 @@ float compare_degrees(float A, float B)
 
 void vm_hw_sped3_update_rot(struct sped3_hardware* hw)
 {
-	if (hw->rot_current != hw->rot_target)
-	{
+    if (hw->rot_current != hw->rot_target)
+    {
         if(abs(hw->rot_target - hw->rot_current) < SPED3_SPEED)
         {
             hw->rot_current = hw->rot_target;
@@ -63,159 +63,159 @@ void vm_hw_sped3_update_rot(struct sped3_hardware* hw)
           
             hw->rot_current = fmod(hw->rot_current, 360);
         }
-	}
-	else hw->state = SPED3_STATE_RUNNING;
+    }
+    else hw->state = SPED3_STATE_RUNNING;
 }
 
 void vm_hw_sped3_set_color(struct sped3_hardware* hw, uint8_t cc, uint8_t intensity)
 {
-	float k = 1.f;
+    float k = 1.f;
 
-	if (intensity == 1)
-		k = 0.5f;
+    if (intensity == 1)
+        k = 0.5f;
 
-	switch (cc)
-	{
-		case 0:
-			glColor3f(0.05f * k, 0.05f * k, 0.05f * k);
-			break;
-		case 1:
-			glColor3f(1.f * k, 0.f, 0.f);
-			break;
-		case 2:
-			glColor3f(0.f, 1.f * k, 0.f);
-			break;
-		case 3:
-			glColor3f(0.f, 0.f, 1.f * k);
-			break;
-	}
+    switch (cc)
+    {
+        case 0:
+            glColor3f(0.05f * k, 0.05f * k, 0.05f * k);
+            break;
+        case 1:
+            glColor3f(1.f * k, 0.f, 0.f);
+            break;
+        case 2:
+            glColor3f(0.f, 1.f * k, 0.f);
+            break;
+        case 3:
+            glColor3f(0.f, 0.f, 1.f * k);
+            break;
+    }
 }
 
 void vm_hw_sped3_cycle(vm_t* vm, uint16_t pos, void* ud)
 {
-	struct sped3_hardware* hw = (struct sped3_hardware*)ud;
-	int i;
-	uint16_t firstword, secondword;
-	uint8_t cc, intensity;
-	float x, y, z;
+    struct sped3_hardware* hw = (struct sped3_hardware*)ud;
+    int i;
+    uint16_t firstword, secondword;
+    uint8_t cc, intensity;
+    float x, y, z;
 
-	glfwMakeContextCurrent(hw->window);
+    glfwMakeContextCurrent(hw->window);
 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0.f, -4.f, 0.f,
-		  0.f, 0.f, 0.f,
-		  0.f, 0.f, 1.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.f, -4.f, 0.f,
+          0.f, 0.f, 0.f,
+          0.f, 0.f, 1.f);
 
-	vm_hw_sped3_update_rot(hw);
+    vm_hw_sped3_update_rot(hw);
 
-	glRotatef(hw->rot_current, 0.f, 0.f, 1.f);
+    glRotatef(hw->rot_current, 0.f, 0.f, 1.f);
 
-	glClearColor(0.f, 0.f, 0.f, 0.f);
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.f, 0.f, 0.f, 0.f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-	glBegin(GL_LINE_STRIP);
-	for (i = 0; i < MIN(hw->num, 128); i++)
-	{
-		firstword  = vm->ram[hw->mem + i * 2];
-		secondword = vm->ram[hw->mem + (i * 2) + 1];
+    glBegin(GL_LINE_STRIP);
+    for (i = 0; i < MIN(hw->num, 128); i++)
+    {
+        firstword  = vm->ram[hw->mem + i * 2];
+        secondword = vm->ram[hw->mem + (i * 2) + 1];
 
-		x = (float)(firstword & 0xff) / 256 * 2 - 1;
-		y = (float)(firstword >> 8) / 256 * 2 - 1;
-		z = (float)(secondword & 0xff) / 256 * 2 - 1;
+        x = (float)(firstword & 0xff) / 256 * 2 - 1;
+        y = (float)(firstword >> 8) / 256 * 2 - 1;
+        z = (float)(secondword & 0xff) / 256 * 2 - 1;
 
-		cc = (secondword >> 8) & 0x3;
-		intensity = secondword >> 11;
+        cc = (secondword >> 8) & 0x3;
+        intensity = secondword >> 11;
 
-		glColor3f(1.f, 1.f, 1.f);
-		vm_hw_sped3_set_color(hw, cc, intensity);
-		glVertex3f(x, y, z);
-	}
-	glEnd();
+        glColor3f(1.f, 1.f, 1.f);
+        vm_hw_sped3_set_color(hw, cc, intensity);
+        glVertex3f(x, y, z);
+    }
+    glEnd();
 
-	glfwSwapBuffers(hw->window);
+    glfwSwapBuffers(hw->window);
 
-	glfwPollEvents();
+    glfwPollEvents();
 }
 
 void vm_hw_sped3_interrupt(vm_t* vm, void* ud)
 {
-	struct sped3_hardware* hw = (struct sped3_hardware*)ud;
+    struct sped3_hardware* hw = (struct sped3_hardware*)ud;
 
-	switch (vm->registers[REG_A])
-	{
-		case SPED3_INTERRUPT_POLL:
-			vm->registers[REG_B] = hw->state;
-			break;
-		case SPED3_INTERRUPT_MAP:
-			hw->mem = vm->registers[REG_X];
-			hw->num = vm->registers[REG_Y];
-			hw->state = SPED3_STATE_RUNNING;
-			break;
-		case SPED3_INTERRUPT_ROTATE:
-			hw->rot_target = vm->registers[REG_X];
-			hw->state = SPED3_STATE_TURNING;
-			break;
-	}
+    switch (vm->registers[REG_A])
+    {
+        case SPED3_INTERRUPT_POLL:
+            vm->registers[REG_B] = hw->state;
+            break;
+        case SPED3_INTERRUPT_MAP:
+            hw->mem = vm->registers[REG_X];
+            hw->num = vm->registers[REG_Y];
+            hw->state = SPED3_STATE_RUNNING;
+            break;
+        case SPED3_INTERRUPT_ROTATE:
+            hw->rot_target = vm->registers[REG_X];
+            hw->state = SPED3_STATE_TURNING;
+            break;
+    }
 }
 
 int vm_hw_sped3_close(GLFWwindow w)
 {
-	void* ud = glfwGetWindowUserPointer(w);
+    void* ud = glfwGetWindowUserPointer(w);
 
-	vm_hw_sped3_free(ud);
-	return 0;
+    vm_hw_sped3_free(ud);
+    return 0;
 }
 
 void vm_hw_sped3_init(vm_t* vm)
 {
-	struct sped3_hardware* hw;
+    struct sped3_hardware* hw;
 
-	hw = malloc(sizeof(struct sped3_hardware));
-	hw->cycle_hook = 0;
-	hw->width = 400;
-	hw->height = 400;
-	hw->mem = 0;
-	hw->num = 0;
-	hw->rot_target = 0;
-	hw->rot_current = 0;
-	hw->state = SPED3_STATE_NO_DATA;
-	hw->vm = vm;
+    hw = malloc(sizeof(struct sped3_hardware));
+    hw->cycle_hook = 0;
+    hw->width = 400;
+    hw->height = 400;
+    hw->mem = 0;
+    hw->num = 0;
+    hw->rot_target = 0;
+    hw->rot_current = 0;
+    hw->state = SPED3_STATE_NO_DATA;
+    hw->vm = vm;
 
-	hw->device.id = 0x42babf3c;
-	hw->device.version = 0x0003;
-	hw->device.manufacturer = 0x1eb37e91;
-	hw->device.handler = &vm_hw_sped3_interrupt;
-	hw->device.userdata = hw;
+    hw->device.id = 0x42babf3c;
+    hw->device.version = 0x0003;
+    hw->device.manufacturer = 0x1eb37e91;
+    hw->device.handler = &vm_hw_sped3_interrupt;
+    hw->device.userdata = hw;
 
-	hw->cycle_hook = vm_hook_register(vm, &vm_hw_sped3_cycle, HOOK_ON_60HZ, hw);
-	hw->hw_id = vm_hw_register(vm, hw->device);
+    hw->cycle_hook = vm_hook_register(vm, &vm_hw_sped3_cycle, HOOK_ON_60HZ, hw);
+    hw->hw_id = vm_hw_register(vm, hw->device);
 
 
-	// TODO: check for errors and cry about it
-	//glfwWindowHint(GLFW_DEPTH_BITS, 16);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	hw->window = (GLFWwindow) glfwCreateWindow(hw->width, hw->height, GLFW_WINDOWED, "SPED-3", NULL);
+    // TODO: check for errors and cry about it
+    //glfwWindowHint(GLFW_DEPTH_BITS, 16);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    hw->window = (GLFWwindow) glfwCreateWindow(hw->width, hw->height, GLFW_WINDOWED, "SPED-3", NULL);
 
-	glfwSetWindowUserPointer(hw->window, hw);
-	glfwMakeContextCurrent(hw->window);
-	glfwSwapInterval(0);
-	
-	glfwSetTime(0.0);
-	glfwSetWindowCloseCallback(&vm_hw_sped3_close);
+    glfwSetWindowUserPointer(hw->window, hw);
+    glfwMakeContextCurrent(hw->window);
+    glfwSwapInterval(0);
+    
+    glfwSetTime(0.0);
+    glfwSetWindowCloseCallback(&vm_hw_sped3_close);
 
-	glViewport(0, 0, hw->width, hw->height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(65.f, (GLfloat) hw->width / (GLfloat) hw->height, 1.f, 1000.f);
+    glViewport(0, 0, hw->width, hw->height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(65.f, (GLfloat) hw->width / (GLfloat) hw->height, 1.f, 1000.f);
 }
 
 void vm_hw_sped3_free(void* ud)
 {
-	struct sped3_hardware* hw = (struct sped3_hardware*)ud;
+    struct sped3_hardware* hw = (struct sped3_hardware*)ud;
 
-	glfwDestroyWindow(hw->window);
-	vm_hook_unregister(hw->vm, hw->cycle_hook);
-	vm_hw_unregister(hw->vm, hw->hw_id);
-	free(hw);
+    glfwDestroyWindow(hw->window);
+    vm_hook_unregister(hw->vm, hw->cycle_hook);
+    vm_hw_unregister(hw->vm, hw->hw_id);
+    free(hw);
 }
