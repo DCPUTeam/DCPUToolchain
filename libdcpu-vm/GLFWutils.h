63689d149bf06e8c36d80db86ebfb9024ca49207
r4d2 <patrick.flick@gmail.com>
2012-10-13 15:41:35 +0200

    Proper freeing on close of LEM and SPED windows.

---
+++ libdcpu-vm/GLFWutils.h
@@ -0,0 +1,16 @@
+/**
+
+    File:           GLFWutils.h
+
+    Project:        DCPU-16 Toolchain
+    Component:      LibDCPU-VM
+
+    Authors:        Patrick Flick
+
+    Description:    Declares utility functions for glfw.
+
+**/
+#include <GL/glfw3.h>
+
+int vm_hw_glfw_close_window_callback(GLFWwindow w);
+void vm_hw_glfw_resize_window_callback(GLFWwindow window, int w, int h);
