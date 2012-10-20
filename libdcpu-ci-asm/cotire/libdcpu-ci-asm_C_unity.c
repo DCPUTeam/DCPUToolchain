dfcb0621492f77595d34f7b20e25152b9b95f8e0
James Rhodes <jrhodes@redpointsoftware.com.au>
2012-10-21 02:41:55 +1100

    Removed cotire cache files from libdcpu-ci-asm.

--- libdcpu-ci-asm/cotire/libdcpu-ci-asm_C_unity.c
+++
@@ -1,8 +0,0 @@
-/* cotire.cmake 1.1.6 generated file */
-/* /Users/me/DCPUToolchain/libdcpu-ci-asm/cotire/libdcpu-ci-asm_C_unity.c */
-#ifndef __cplusplus
-#include "/Users/me/DCPUToolchain/libdcpu-ci-asm/aout.c"
-#include "/Users/me/DCPUToolchain/libdcpu-ci-asm/assem.c"
-#include "/Users/me/DCPUToolchain/libdcpu-ci-asm/textn.c"
-#include "/Users/me/DCPUToolchain/libdcpu-ci-asm/treloc.c"
-#endif
