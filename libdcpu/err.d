0e45f3d85e55ae5b9d4befc5021294e11f263d54
r4d2 <patrick.flick@gmail.com>
2012-11-01 12:01:25 +0100

    error handleing in case OpenGL fails

--- libdcpu/err.d
+++ libdcpu/err.d
@@ -37,6 +37,7 @@ ERR_INVALID_TARGET_NAME                              invalid-target-name        
 ERR_BIN_LOAD_FAILED                                  bin-load-failed                        "failed to load bins from the object file '%s'."
 ERR_KERNEL_ARGUMENT_NOT_ALLOWED                      kernel-argument-not-allowed            "the -k argument can not be used when building a kernel."
 ERR_JUMPLIST_FILE_REQUIRED                           jump-list-required                     "a jumplist is required when linking against a kernel."
+ERR_COULD_NOT_CREATE_OPENGL_CONTEXT                  failed-create-opengl-context           "failed to create an OpenGL context (check your graphic drivers).\n"
 
 WARN_SKIPPING_SHORT_LITERALS_TYPE                    short-literals-skipped-type            "skipping short literal compression due to target type."
 WARN_SKIPPING_SHORT_LITERALS_REQUEST                 short-literals-skipped-request         "skipping short literal compression on request."
