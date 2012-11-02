ff880027145695e37fa906e309101764781b84a6
James Rhodes <jrhodes@redpointsoftware.com.au>
2012-11-03 04:04:03 +1100

    Documented more of libdcpu/dcpu.h.

--- libdcpu/dcpu.h
+++ libdcpu/dcpu.h
@@ -50,82 +50,82 @@ typedef uint8_t bool;
 /// @{
 ///
 
-//< The A register of the DCPU.
+/// The A register of the DCPU.
 #define REG_A       0x00
-//< The B register of the DCPU.
+/// The B register of the DCPU.
 #define REG_B       0x01
-//< The C register of the DCPU.
+/// The C register of the DCPU.
 #define REG_C       0x02
-//< The X register of the DCPU.
+/// The X register of the DCPU.
 #define REG_X       0x03
-//< The Y register of the DCPU.
+/// The Y register of the DCPU.
 #define REG_Y       0x04
-//< The Z register of the DCPU.
+/// The Z register of the DCPU.
 #define REG_Z       0x05
-//< The I register of the DCPU.
+/// The I register of the DCPU.
 #define REG_I       0x06
-//< The J register of the DCPU.
+/// The J register of the DCPU.
 #define REG_J       0x07
 
-//< The value of the address in the A register.
+/// The value of the address in the A register.
 #define VAL_A       0x08
-//< The value of the address in the B register.
+/// The value of the address in the B register.
 #define VAL_B       0x09
-//< The value of the address in the C register.
+/// The value of the address in the C register.
 #define VAL_C       0x0A
-//< The value of the address in the X register.
+/// The value of the address in the X register.
 #define VAL_X       0x0B
-//< The value of the address in the Y register.
+/// The value of the address in the Y register.
 #define VAL_Y       0x0C
-//< The value of the address in the Z register.
+/// The value of the address in the Z register.
 #define VAL_Z       0x0D
-//< The value of the address in the I register.
+/// The value of the address in the I register.
 #define VAL_I       0x0E
-//< The value of the address in the J register.
+/// The value of the address in the J register.
 #define VAL_J       0x0F
 
-//< The value of the address in the A register, plus the value of the next word.
+/// The value of the address in the A register, plus the value of the next word.
 #define NXT_VAL_A   0x10
-//< The value of the address in the B register, plus the value of the next word.
+/// The value of the address in the B register, plus the value of the next word.
 #define NXT_VAL_B   0x11
-//< The value of the address in the C register, plus the value of the next word.
+/// The value of the address in the C register, plus the value of the next word.
 #define NXT_VAL_C   0x12
-//< The value of the address in the X register, plus the value of the next word.
+/// The value of the address in the X register, plus the value of the next word.
 #define NXT_VAL_X   0x13
-//< The value of the address in the Y register, plus the value of the next word.
+/// The value of the address in the Y register, plus the value of the next word.
 #define NXT_VAL_Y   0x14
-//< The value of the address in the Z register, plus the value of the next word.
+/// The value of the address in the Z register, plus the value of the next word.
 #define NXT_VAL_Z   0x15
-//< The value of the address in the I register, plus the value of the next word.
+/// The value of the address in the I register, plus the value of the next word.
 #define NXT_VAL_I   0x16
-//< The value of the address in the J register, plus the value of the next word.
+/// The value of the address in the J register, plus the value of the next word.
 #define NXT_VAL_J   0x17
 
-//< Represents either PUSH or POP, depending on which parameter it is used in.
+/// Represents either PUSH or POP, depending on which parameter it is used in.
 #define PUSH_POP    0x18
-//< Represents PEEK, which is the value of the address in the SP register.
+/// Represents PEEK, which is the value of the address in the SP register.
 #define PEEK        0x19
-//< Represents PICK, which is the value of the address pointed to by SP plus the next word.
+/// Represents PICK, which is the value of the address pointed to by SP plus the next word.
 #define PICK        0x1A
-//< The SP register of the DCPU, which points to the top of the stack.
+/// The SP register of the DCPU, which points to the top of the stack.
 #define SP          0x1B
-//< The PC register of the DCPU, which points to the current instruction.
+/// The PC register of the DCPU, which points to the current instruction.
 #define PC          0x1C
-//< The EX register of the DCPU, which holds overflow and underflow information.
+/// The EX register of the DCPU, which holds overflow and underflow information.
 #define EX          0x1D
-//< The value stored in the address indicated in the next word.
+/// The value stored in the address indicated in the next word.
 #define NXT         0x1E
-//< The value of the next word.
+/// The value of the next word.
 #define NXT_LIT     0x1F
 
-//< The interrupt address (can not be used directly in assembly).
+/// The interrupt address (can not be used directly in assembly).
 #define IA          0xFF
 
-//< Indicates no position in the call.
+/// Indicates no position in the call.
 #define POS__      0x0
-//< Indicates the value is in the 'a' parameter.
+/// Indicates the value is in the 'a' parameter.
 #define POS_A      0x1
-//< Indicates the value is in the 'b' parameter.
+/// Indicates the value is in the 'b' parameter.
 #define POS_B      0x2
 
 ///
@@ -138,64 +138,64 @@ typedef uint8_t bool;
 /// @{
 ///
 
-//< Indicates a non-basic opcode.
+/// Indicates a non-basic opcode.
 #define OP_NONBASIC   0x00
-//< The SET instruction, which assigns the second value to the first.
+/// The SET instruction, which assigns the second value to the first.
 #define OP_SET        0x01
-//< The ADD instruction, which adds the second value onto the first.
+/// The ADD instruction, which adds the second value onto the first.
 #define OP_ADD        0x02
-//< The SUB instruction, which subtracts the second value from the first.
+/// The SUB instruction, which subtracts the second value from the first.
 #define OP_SUB        0x03
-//< The MUL instruction, which multiplies the second value by the first and assigns the result to the first.
+/// The MUL instruction, which multiplies the second value by the first and assigns the result to the first.
 #define OP_MUL        0x04
-//< The MLI instruction, which is a signed MUL instruction.
+/// The MLI instruction, which is a signed MUL instruction.
 #define OP_MLI        0x05
-//< The DIV instruction, which divides the first value by the second and assigns the result to the first.
+/// The DIV instruction, which divides the first value by the second and assigns the result to the first.
 #define OP_DIV        0x06
-//< The DVI instruction, which is a signed DIV instruction.
+/// The DVI instruction, which is a signed DIV instruction.
 #define OP_DVI        0x07
-//< The MOD instruction, which takes the modulo of the first value over the second, and assigns the result to the first.
+/// The MOD instruction, which takes the modulo of the first value over the second, and assigns the result to the first.
 #define OP_MOD        0x08
-//< The MDI instruction, which is a signed MOD instruction.
+/// The MDI instruction, which is a signed MOD instruction.
 #define OP_MDI        0x09
-//< The AND instruction, which assigns the bitwise AND of the first and second values to the first value.
+/// The AND instruction, which assigns the bitwise AND of the first and second values to the first value.
 #define OP_AND        0x0A
-//< The BOR instruction, which assigns the bitwise OR of the first and second values to the first value.
+/// The BOR instruction, which assigns the bitwise OR of the first and second values to the first value.
 #define OP_BOR        0x0B
-//< The XOR instruction, which assigns the bitwise XOR of the first and second values to the first value.
+/// The XOR instruction, which assigns the bitwise XOR of the first and second values to the first value.
 #define OP_XOR        0x0C
-//< The SHR instruction, which is a shift-right operation of the first and second values.
+/// The SHR instruction, which is a shift-right operation of the first and second values.
 #define OP_SHR        0x0D
-//< The ASR instruction, which is a arithmetic shift-right operation of the first and second values.
+/// The ASR instruction, which is a arithmetic shift-right operation of the first and second values.
 #define OP_ASR        0x0E
-//< The SHL instruction, which is a shift-left operation of the first and second values.
+/// The SHL instruction, which is a shift-left operation of the first and second values.
 #define OP_SHL        0x0F
-//< The IFB instruction, which performs the next instruction if (b&a)!=0.
+/// The IFB instruction, which performs the next instruction if (b&a)!=0.
 #define OP_IFB        0x10
-//< The IFC instruction, which performs the next instruction if (b&a)==0.
+/// The IFC instruction, which performs the next instruction if (b&a)==0.
 #define OP_IFC        0x11
-//< The IFE instruction, which performs the next instruction if b==a.
+/// The IFE instruction, which performs the next instruction if b==a.
 #define OP_IFE        0x12
-//< The IFN instruction, which performs the next instruction if b!=a.
+/// The IFN instruction, which performs the next instruction if b!=a.
 #define OP_IFN        0x13
-//< The IFG instruction, which performs the next instruction if b>a.
+/// The IFG instruction, which performs the next instruction if b>a.
 #define OP_IFG        0x14
-//< The IFA instruction, which performs the next instruction if b>a (signed).
+/// The IFA instruction, which performs the next instruction if b>a (signed).
 #define OP_IFA        0x15
-//< The IFL instruction, which performs the next instruction if b<a.
+/// The IFL instruction, which performs the next instruction if b<a.
 #define OP_IFL        0x16
-//< The IFU instruction, which performs the next instruction if b<a (signed).
+/// The IFU instruction, which performs the next instruction if b<a (signed).
 #define OP_IFU        0x17
-//< The ADX instruction, see the DCPU-16 specification for semantics.
+/// The ADX instruction, see the DCPU-16 specification for semantics.
 #define OP_ADX        0x1A
-//< The SBX instruction, see the DCPU-16 specification for semantics.
+/// The SBX instruction, see the DCPU-16 specification for semantics.
 #define OP_SBX        0x1B
-//< The STI instruction, which sets the second value to the first and increases the I and J registers.
+/// The STI instruction, which sets the second value to the first and increases the I and J registers.
 #define OP_STI        0x1E
-//< The STD instruction, which sets the second value to the first and decreases the I and J registers.
+/// The STD instruction, which sets the second value to the first and decreases the I and J registers.
 #define OP_STD        0x1F
 
-//< The reserved non-basic instruction for future expansion.
+/// The reserved non-basic instruction for future expansion.
 #define NBOP_RESERVED 0x00
 #define NBOP_JSR      0x01
 #define NBOP_INT      0x08
@@ -220,16 +220,16 @@ typedef uint8_t bool;
 // Interrupt messages
 #define INT_TIMER     0x0
 
-//< Combines the parameters into the 16-bit unsigned integer for the instruction.
+/// Combines the parameters into the 16-bit unsigned integer for the instruction.
 #define INSTRUCTION_CREATE(op, b, a) (((a & 0x3f) << 10) + ((b & 0x1f) << 5) + (op & 0x1f))
-//< Retrieves the 'a' parameter from an instruction.
+/// Retrieves the 'a' parameter from an instruction.
 #define INSTRUCTION_GET_A(inst) ((inst & 0xfc00) >> 10)
-//< Retrieves the 'b' parameter from an instruction.
+/// Retrieves the 'b' parameter from an instruction.
 #define INSTRUCTION_GET_B(inst) ((inst & 0x3e0) >> 5)
-//< Retrieves the basic opcode from an instruction.
+/// Retrieves the basic opcode from an instruction.
 #define INSTRUCTION_GET_OP(inst) (inst & 0x1f)
 
-//< Defines the maximum number of interrupts permitted on the DCPU.
+/// Defines the maximum number of interrupts permitted on the DCPU.
 #define INTERRUPT_MAX    255
 
 // Timing and Frequency settings
