0bd32ee8893216c458b8a9b684134be8367b59c2
James Rhodes <jrhodes@redpointsoftware.com.au>
2012-11-03 23:55:44 +1100

    Added initial policy files.

---
+++ libdcpu-ld-policy/policy.h
@@ -0,0 +1,19 @@
+///
+/// @addtogroup LibDCPU-LD-Policy
+/// @brief Library for reading and executing linker policies.
+///
+/// This library parses linker policies (scripts) and provides the ability to
+/// register actions when encountering components of scripts.
+///
+/// @{
+///
+/// @file
+/// @brief General API access for linker policies.
+/// @author James Rhodes
+/// 
+/// This header provides general access to the linker policy parsing API.
+///
+
+///
+/// @}
+///
