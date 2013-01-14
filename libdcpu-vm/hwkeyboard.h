b3cfe16779159604e13cb7b36df1b8314b465cd5
Kevin <psycocoffey@gmail.com>
2013-01-14 18:52:15 -0500

    Fixed off by one error in pressed array
    #defines give meaning to constant values

--- libdcpu-vm/hwkeyboard.h
+++ libdcpu-vm/hwkeyboard.h
@@ -17,12 +17,23 @@
 #include "vm.h"
 
 #define KB_BUFFER_COUNT    100
-#define KB_PRESSED_COUNT   0x91
+#define KB_PRESSED_COUNT   0x92
 
 #define KB_INTERRUPT_CLEAR 0
 #define KB_INTERRUPT_STORE 1
 #define KB_INTERRUPT_TEST  2
 #define KB_INTERRUPT_MSG   3
+
+#define KB_KEY_BACKSPACE 0x10
+#define KB_KEY_ENTER     0x11
+#define KB_KEY_INSERT    0x12
+#define KB_KEY_DELETE    0x13
+#define KB_KEY_UP        0x80
+#define KB_KEY_DOWN      0x81
+#define KB_KEY_LEFT      0x82
+#define KB_KEY_RIGHT     0x83
+#define KB_KEY_SHIFT     0x90
+#define KB_KEY_CONTROL   0x91
 
 struct keyboard_hardware
 {
@@ -36,6 +47,12 @@ struct keyboard_hardware
     uint16_t buffer_idx_w;
 
     bool* pressed;
+    // Left and right modifiers need to be tracked seperately
+    bool control_left;
+    bool control_right;
+    bool shift_left;
+    bool shift_right;
+
 
     uint16_t interrupt_message;
     bool lowercase;
