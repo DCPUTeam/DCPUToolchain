cfbaf75eb32d706aaf359c8928a13a8e12e29e2b
Kevin <psycocoffey@gmail.com>
2013-01-14 18:55:26 -0500

    Keyboard events now make sense
    Letter states are unified for capital and lower cases.
    Shift and Control now consider both of their keys when determining their state.
    Added range checks for array accesses.
    Use new #defines for key IDs
    Fixed several compiler warnings.

--- libdcpu-vm/hwkeyboard.c
+++ libdcpu-vm/hwkeyboard.c
@@ -29,24 +29,28 @@ struct keyboard_hardware* g_hw;
 
 void vm_hw_keyboard_append_to_buffer(struct keyboard_hardware* hw, int index)
 {
-    hw->buffer[hw->buffer_idx_w++] = index;
-    // Should the write index wrap back to the beginning?
-    if (hw->buffer_idx_w >= KB_BUFFER_COUNT)
+    // Sanity checking
+    if (index >= 0)
     {
-        hw->buffer_idx_w = 0;
-    }
-
-    // Has the write index caught up with the read index?
-    if (hw->buffer_idx_r == hw->buffer_idx_w)
-    {
-        // The oldest character has been discarded.
-        // Advance the read index to compensate.
-        hw->buffer_idx_r++;
-
-        // Should the read index wrap back to the beginning?
-        if (hw->buffer_idx_r >= KB_BUFFER_COUNT)
+        hw->buffer[hw->buffer_idx_w++] = (uint16_t) index;
+        // Should the write index wrap back to the beginning?
+        if (hw->buffer_idx_w >= KB_BUFFER_COUNT)
         {
-            hw->buffer_idx_r = 0;
+            hw->buffer_idx_w = 0;
+        }
+
+        // Has the write index caught up with the read index?
+        if (hw->buffer_idx_r == hw->buffer_idx_w)
+        {
+            // The oldest character has been discarded.
+            // Advance the read index to compensate.
+            hw->buffer_idx_r++;
+
+            // Should the read index wrap back to the beginning?
+            if (hw->buffer_idx_r >= KB_BUFFER_COUNT)
+            {
+                hw->buffer_idx_r = 0;
+            }
         }
     }
 }
@@ -54,67 +58,77 @@ void vm_hw_keyboard_append_to_buffer(struct keyboard_hardware* hw, int index)
 
 void vm_hw_keyboard_handle_char(GLFWwindow w, int c)
 {
-    if (c > 0x20 || c < 0x7f)
+    // Sanity checking
+    if ( 0 <= c && c < KB_PRESSED_COUNT)
         vm_hw_keyboard_append_to_buffer(g_hw, c);
 }
 
 void vm_hw_keyboard_handle_key(GLFWwindow w, int key, int state)
 {
-    uint16_t keycode = 0;
-    uint16_t index = 0;
+    bool key_state = (bool) state;
+    int keycode = 0;
+    int index = 0;
 
     switch (key)
     {
         case GLFW_KEY_BACKSPACE:
-            g_hw->pressed[0x10] = state;
-            index = 0x10;
+            g_hw->pressed[KB_KEY_BACKSPACE] = key_state;
+            index = KB_KEY_BACKSPACE;
             break;
-	case 335:    /* numpad enter */
+	    case GLFW_KEY_KP_ENTER:    /* numpad enter */
         case GLFW_KEY_ENTER:
-            g_hw->pressed[0x11] = state;
-            index = 0x11;
+            g_hw->pressed[KB_KEY_ENTER] = key_state;
+            index = KB_KEY_ENTER;
             break;
         case GLFW_KEY_INSERT:
-            g_hw->pressed[0x12] = state;
-            index = 0x12;
+            g_hw->pressed[KB_KEY_INSERT] = key_state;
+            index = KB_KEY_INSERT;
             break;
         case GLFW_KEY_DELETE:
-            g_hw->pressed[0x13] = state;
-            index = 0x13;
+            g_hw->pressed[KB_KEY_DELETE] = key_state;
+            index = KB_KEY_DELETE;
             break;
 
         case GLFW_KEY_UP:
-            g_hw->pressed[0x80] = state;
-            index = 0x80;
+            g_hw->pressed[KB_KEY_UP] = key_state;
+            index = KB_KEY_UP;
             break;
         case GLFW_KEY_DOWN:
-            g_hw->pressed[0x81] = state;
-            index = 0x81;
+            g_hw->pressed[KB_KEY_DOWN] = key_state;
+            index = KB_KEY_DOWN;
             break;
         case GLFW_KEY_LEFT:
-            g_hw->pressed[0x82] = state;
-            index = 0x82;
+            g_hw->pressed[KB_KEY_LEFT] = key_state;
+            index = KB_KEY_LEFT;
             break;
         case GLFW_KEY_RIGHT:
-            g_hw->pressed[0x83] = state;
-            index = 0x83;
+            g_hw->pressed[KB_KEY_RIGHT] = key_state;
+            index = KB_KEY_RIGHT;
             break;
 
         case GLFW_KEY_LEFT_CONTROL:
-        case GLFW_KEY_RIGHT_CONTROL:
-            g_hw->pressed[0x91] = state;
-            index = 0x91;
+            g_hw->control_left = key_state;
+            g_hw->pressed[KB_KEY_CONTROL] = g_hw->control_left || g_hw->control_right;
+            index = KB_KEY_CONTROL;
             break;
-
+        case GLFW_KEY_RIGHT_CONTROL:
+            g_hw->control_right = key_state;
+            g_hw->pressed[KB_KEY_CONTROL] = g_hw->control_left || g_hw->control_right;
+            index = KB_KEY_CONTROL;
+            break;
         case GLFW_KEY_LEFT_SHIFT:
+            g_hw->shift_left = key_state;
+            g_hw->pressed[KB_KEY_SHIFT] = g_hw->shift_left || g_hw->shift_right;
+            index = KB_KEY_SHIFT;
+            break;
         case GLFW_KEY_RIGHT_SHIFT:
-            g_hw->pressed[0x90] = state;
-            g_hw->lowercase = !state;
-            index = 0x90;
+            g_hw->shift_right = key_state;
+            g_hw->pressed[KB_KEY_SHIFT] = g_hw->shift_left || g_hw->shift_right;
+            index = KB_KEY_SHIFT;
             break;
 
         case GLFW_KEY_F8:
-            if (state)
+            if (key_state)
             {
                 g_hw->vm->radiation = !g_hw->vm->radiation;
                 printf("DCPU radiation: %s\n", g_hw->vm->radiation ? "ACTIVE" : "INACTIVE");
@@ -122,23 +136,23 @@ void vm_hw_keyboard_handle_key(GLFWwindow w, int key, int state)
             break;
 
         default:
-            if (g_hw->lowercase)
-            {
-                keycode = tolower(key);
-            }
-            else
-            {
-                keycode = key;
-            }
+            // Letter keys use the uppercase values
+            index = key;
+            if(index < KB_PRESSED_COUNT)
+                g_hw->pressed[index] = key_state;
 
-            g_hw->pressed[keycode] = state;
-            index = keycode;
+            // Have the lowercase letters mirror the state
+            // to make the behavior more intuitive.
+            keycode = tolower(key);
+            if(keycode < KB_PRESSED_COUNT)
+                g_hw->pressed[keycode] = key_state;
+
             break;
     }
 
     // Char events are handled by the char handler.
     // No index should be larger than 0x91: Control
-    if (state == true && (index < 0x20 || index > 0x7f) && index < 0x91)
+    if (key_state == true && (index < 0x20 || index > 0x7f) && index < KB_PRESSED_COUNT)
     {
         vm_hw_keyboard_append_to_buffer(g_hw, index);
     }
@@ -207,7 +221,12 @@ void vm_hw_keyboard_init(vm_t* vm)
     
     hw->pressed = malloc(sizeof(bool) * KB_PRESSED_COUNT);
     // Initialize all keys to released
-    memset(hw->pressed, GLFW_RELEASE, sizeof(bool) * KB_PRESSED_COUNT);
+    memset(hw->pressed, (bool) GLFW_RELEASE, sizeof(bool) * KB_PRESSED_COUNT);
+
+    hw->control_left = (bool) GLFW_RELEASE;
+    hw->control_right = (bool) GLFW_RELEASE;
+    hw->shift_left = (bool) GLFW_RELEASE;
+    hw->shift_right = (bool) GLFW_RELEASE;
     
     hw->interrupt_message = 0;
     hw->lowercase = true;
